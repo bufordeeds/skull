be8019c55f986446da9b4023048576eb6a82ade2
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-05-19 02:30:25 -0500

    refactor: SKU-6 extend TextInput atom to suppor email and password

--- client/app/components/atoms/TextInput.tsx
+++ client/app/components/atoms/TextInput.tsx
@@ -1,12 +1,55 @@
 import {
   TextInput as PaperTextInput,
   TextInputProps as PaperTextInputProps,
+  TextInputIconProps,
 } from "react-native-paper";
-
+import { useState } from "react";
 export interface TextInputProps extends PaperTextInputProps {
-  label: string;
+  label?: string;
+  kind?: "password" | "email" | "text";
 }
 
-export const TextInput = (props: TextInputProps) => {
-  return <PaperTextInput {...props} />;
+const PasswordInput = (props: TextInputProps) => {
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
+  return (
+    <PaperTextInput
+      {...props}
+      label={props.label || "Password"}
+      secureTextEntry={secureTextEntry}
+      right={
+        <PaperTextInput.Icon
+          icon={secureTextEntry ? "eye" : "eye-off"}
+          onPress={() => setSecureTextEntry(!secureTextEntry)}
+        />
+      }
+    />
+  );
+};
+
+const EmailInput = (props: TextInputProps) => {
+  return (
+    <PaperTextInput
+      label={props.label || "Email"}
+      keyboardType="email-address"
+      autoCapitalize="none"
+    />
+  );
+};
+
+export const TextInput = ({
+  kind,
+  label = "Text",
+  ...props
+}: TextInputProps) => {
+  switch (kind) {
+    case "password":
+      return <PasswordInput {...props} />;
+    case "email":
+      return <EmailInput {...props} />;
+  }
+  return <PaperTextInput {...props} label={label} />;
+};
+
+TextInput.Icon = (props: TextInputIconProps) => {
+  return <PaperTextInput.Icon {...props} />;
 };

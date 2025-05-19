8719b82b8ecc2e4df8281dc5a8345be9ab6c829f
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-05-19 01:01:10 -0500

    feat: SKU-6 add Main wrapper component

---
+++ client/app/components/atoms/Main.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from "react-native";
+
+export const Main = ({ children }: { children: React.ReactNode }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    padding: 20,
+  },
+});

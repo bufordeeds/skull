57722055de3c6e69e7a852a589609d5f47d53f25
Robert Collins <58490986+robkebab@users.noreply.github.com>
2025-05-21 22:33:38 -0500

    feat: SKU-6 add base LoginPage

---
+++ client/app/components/pages/WelcomePage.tsx
@@ -0,0 +1,10 @@
+import { Main } from "../atoms/Main";
+import { Text } from "../atoms/Text";
+
+export default function WelcomePage() {
+  return (
+    <Main>
+      <Text>Welcome</Text>
+    </Main>
+  );
+}
